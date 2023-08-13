4879f42337410f127797d2b18ca037407492889a
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2023-08-13 22:13:47 +0300

    Add authorities converter

--- rest-service/src/main/java/com/dmit/config/SecurityConfig.java
+++ rest-service/src/main/java/com/dmit/config/SecurityConfig.java
@@ -1,22 +1,88 @@
 package com.dmit.config;
 
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.stream.Stream;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users").hasAuthority("USER")
                         .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
 
         return http.build();
+    }
+
+    @RequiredArgsConstructor
+    static class JwtGrantedAuthoritiesConverter implements Converter<Jwt, Collection<? extends GrantedAuthority>> {
+        @Override
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+        public Collection<? extends GrantedAuthority> convert(Jwt jwt) {
+            return Stream.of("$.realm_access.roles", "$.resource_access.*.roles").flatMap(claimPaths -> {
+                        Object claim;
+                        try {
+                            claim = JsonPath.read(jwt.getClaims(), claimPaths);
+                        } catch (PathNotFoundException e) {
+                            claim = null;
+                        }
+                        if (claim == null) {
+                            return Stream.empty();
+                        }
+                        if (claim instanceof String claimStr) {
+                            return Stream.of(claimStr.split(","));
+                        }
+                        if (claim instanceof String[] claimArr) {
+                            return Stream.of(claimArr);
+                        }
+                        if (Collection.class.isAssignableFrom(claim.getClass())) {
+                            final var iter = ((Collection) claim).iterator();
+                            if (!iter.hasNext()) {
+                                return Stream.empty();
+                            }
+                            final var firstItem = iter.next();
+                            if (firstItem instanceof String) {
+                                return (Stream<String>) ((Collection) claim).stream();
+                            }
+                            if (Collection.class.isAssignableFrom(firstItem.getClass())) {
+                                return (Stream<String>) ((Collection) claim).stream().flatMap(colItem -> ((Collection) colItem).stream()).map(String.class::cast);
+                            }
+                        }
+                        return Stream.empty();
+                    })
+                    /* Insert some transformation here if you want to add a prefix like "ROLE_" or force upper-case authorities */
+                    .map(SimpleGrantedAuthority::new)
+                    .map(GrantedAuthority.class::cast).toList();
+        }
+    }
+
+    @Component
+    @RequiredArgsConstructor
+    static class SpringAddonsJwtAuthenticationConverter implements Converter<Jwt, JwtAuthenticationToken> {
+        @Override
+        public JwtAuthenticationToken convert(Jwt jwt) {
+            final var authorities = new JwtGrantedAuthoritiesConverter().convert(jwt);
+            final String username = JsonPath.read(jwt.getClaims(), "preferred_username");
+            return new JwtAuthenticationToken(jwt, authorities, username);
+        }
     }
 }
